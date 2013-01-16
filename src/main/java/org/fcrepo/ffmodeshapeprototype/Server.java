ca9fd6253ddc49b807d38b8ebf1b6d277f58f35c
ajs6f <ajs6f@virginia.edu>
2013-01-16 14:33:29 -0500

    Moved Workspace into static state, as experimental mode of repo management

--- src/main/java/org/fcrepo/ffmodeshapeprototype/Server.java
+++ src/main/java/org/fcrepo/ffmodeshapeprototype/Server.java
@@ -1,42 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.fcrepo.ffmodeshapeprototype;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.util.logging.Level;
 
-import org.modeshape.common.i18n.TextI18n;
-import org.modeshape.common.logging.Logger;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.jcr.Workspace;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.modeshape.common.collection.Problems;
-import org.infinispan.schematic.document.ParsingException;
-import org.modeshape.jcr.ConfigurationException;
-import org.modeshape.jcr.JcrRepository;
-import org.modeshape.jcr.ModeShapeEngine;
-import org.modeshape.jcr.RepositoryConfiguration;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import org.modeshape.jcr.JcrSession;
+
+import org.infinispan.schematic.document.ParsingException;
+import org.modeshape.common.collection.Problems;
+import org.modeshape.common.logging.Logger;
+import org.modeshape.jcr.ConfigurationException;
+import org.modeshape.jcr.JcrRepository;
+import org.modeshape.jcr.ModeShapeEngine;
+import org.modeshape.jcr.RepositoryConfiguration;
 
 /**
  * 
  * @author cabeer
+ * @author ajs6f
  */
 
 @Path("/")
@@ -45,38 +33,42 @@ public class Server {
 	private JcrRepository repository;
 	private final Logger logger = Logger.getLogger(Server.class.getName());
 
+	static private Workspace ws = null;
+
 	public Server() throws Exception, ConfigurationException,
 			RepositoryException {
-		RepositoryConfiguration repository_config = null;
-		try {
-			repository_config = RepositoryConfiguration
-					.read("my_repository.json");
-			Problems problems = repository_config.validate();
+		if (ws == null) {
+			RepositoryConfiguration repository_config = null;
+			try {
+				repository_config = RepositoryConfiguration
+						.read("my_repository.json");
+				Problems problems = repository_config.validate();
 
-			if (problems.hasErrors()) {
-				System.err.println("Problems starting the engine.");
-				System.err.println(problems);
-				throw new Exception("Problems starting the engine.");
+				if (problems.hasErrors()) {
+					System.err.println("Problems starting the engine.");
+					System.err.println(problems);
+					throw new Exception("Problems starting the engine.");
+				}
+
+			} catch (ParsingException ex) {
+				logger.error(ex, null, (Object) null);
+			} catch (FileNotFoundException ex) {
+				logger.error(ex, null, (Object) null);
 			}
 
-		} catch (ParsingException ex) {
-			logger.error(ex, null, (Object) null);
-		} catch (FileNotFoundException ex) {
-			logger.error(ex, null, (Object) null);
-		}
+			this.engine = new ModeShapeEngine();
 
-		this.engine = new ModeShapeEngine();
-
-		if (this.engine == null || repository_config == null) {
-			throw new Exception("Missing engine");
+			if (this.engine == null || repository_config == null) {
+				throw new Exception("Missing engine");
+			}
+			logger.debug("Started ModeShape engine.\n");
+			engine.start();
+			this.repository = engine.deploy(repository_config);
+			logger.debug("Deployed repository.");
+			ws = this.repository.login().getWorkspace();
+			ws.createWorkspace("fedora");
+			logger.debug("Created 'fedora' workspace.\n");
 		}
-		logger.debug("Started ModeShape engine.\n");
-		engine.start();
-		this.repository = engine.deploy(repository_config);
-		logger.debug("Deployed repository {}.", repository.getName());
-		Workspace ws = this.repository.login().getWorkspace();
-		ws.createWorkspace("fedora");
-		logger.debug("Created 'fedora' workspace.\n");
 	}
 
 	@GET
@@ -89,7 +81,7 @@ public class Server {
 	@Path("/objects/{pid}")
 	public Response ingest(@PathParam("pid") String pid)
 			throws RepositoryException {
-		JcrSession session = this.repository.login("fedora");
+		Session session = ws.getSession();
 
 		Node root = session.getRootNode();
 		if (session.hasPermission("/" + pid, "add_node")) {
@@ -106,7 +98,7 @@ public class Server {
 	@Path("/objects/{pid}")
 	public Response getObject(@PathParam("pid") String pid)
 			throws RepositoryException {
-		JcrSession session = this.repository.login("fedora");
+		Session session = ws.getSession();
 		Node root = session.getRootNode();
 
 		if (root.hasNode(pid)) {
@@ -120,7 +112,7 @@ public class Server {
 	@GET
 	@Path("/objects")
 	public Response getObjects() throws RepositoryException {
-		JcrSession session = this.repository.login("fedora");
+		Session session = ws.getSession();
 		Node root = session.getRootNode();
 
 		StringBuffer nodes = new StringBuffer();
