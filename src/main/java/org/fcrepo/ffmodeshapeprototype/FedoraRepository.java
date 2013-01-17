122e9a738ea14314e31a70ad888009ea3c7ccb1a
ajs6f <ajs6f@virginia.edu>
2013-01-17 11:00:25 -0500

    Added XML version of namespaces endpoint

--- src/main/java/org/fcrepo/ffmodeshapeprototype/FedoraRepository.java
+++ src/main/java/org/fcrepo/ffmodeshapeprototype/FedoraRepository.java
@@ -5,11 +5,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import org.modeshape.common.logging.Logger;
 import org.modeshape.jcr.ConfigurationException;
 
+import com.google.common.collect.ImmutableMap;
+
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
 import java.net.URI;
 
 /**
@@ -51,32 +57,51 @@ public class FedoraRepository extends AbstractResource {
 
 	}
 
-    @POST
-    @Path("/namespaces/{ns}")
-    public Response registerObjectNamespace(@PathParam("ns") String ns) throws RepositoryException {
-        Session session = ws.getSession();
-        Workspace w = session.getWorkspace();
-        NamespaceRegistry r = w.getNamespaceRegistry();
+	@POST
+	@Path("/namespaces/{ns}")
+	public Response registerObjectNamespace(@PathParam("ns") String ns)
+			throws RepositoryException {
+		Session session = ws.getSession();
+		Workspace w = session.getWorkspace();
+		NamespaceRegistry r = w.getNamespaceRegistry();
 
-        r.registerNamespace(ns,"info:fedora/" + ns);
+		r.registerNamespace(ns, "info:fedora/" + ns);
 
-        return Response.ok().entity(ns).build();
-    }
+		return Response.ok().entity(ns).build();
+	}
 
-    @GET
-    @Path("/namespaces")
-    public Response getObjectNamespaces() throws RepositoryException {
-        Session session = ws.getSession();
-        Workspace w = session.getWorkspace();
-        NamespaceRegistry r = w.getNamespaceRegistry();
+	@GET
+	@Path("/namespaces")
+	public Response getObjectNamespaces() throws RepositoryException {
+		Session session = ws.getSession();
+		Workspace w = session.getWorkspace();
+		NamespaceRegistry r = w.getNamespaceRegistry();
 
-        StringBuffer out =        new StringBuffer();
-        String[] uris =  r.getURIs();
-        String[] prefixes = r.getPrefixes();
-        for(int i = 0; i < uris.length; i++) {
-            out.append(prefixes[i] + " : " + uris[i] + "\n");
-        }
+		StringBuffer out = new StringBuffer();
+		String[] uris = r.getURIs();
+		String[] prefixes = r.getPrefixes();
+		for (int i = 0; i < uris.length; i++) {
+			out.append(prefixes[i] + " : " + uris[i] + "\n");
+		}
 
-        return Response.ok().entity(out.toString()).build();
-    }
+		return Response.ok().entity(out.toString()).build();
+	}
+
+	@GET
+	@Path("/namespaces")
+	@Produces("text/xml")
+	public Response getObjectNamespacesInXML() throws RepositoryException,
+			IOException, TemplateException {
+		final NamespaceRegistry reg = ws.getNamespaceRegistry();
+		ImmutableMap.Builder<String, Object> b = ImmutableMap.builder();
+		for (String prefix : reg.getPrefixes()) {
+			b.put(prefix, reg.getURI(prefix));
+		}
+		return Response
+				.ok()
+				.entity(renderTemplate("namespaceRegistry.ftl",
+						ImmutableMap.of("namespaces", (Object) b.build())))
+				.build();
+	}
+
 }
