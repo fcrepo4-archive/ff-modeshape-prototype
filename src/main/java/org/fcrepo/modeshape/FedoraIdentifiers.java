92bfb1e5515188c3d088f0bb344ad2316c55907a
ajs6f <ajs6f@virginia.edu>
2013-02-04 17:07:07 -0500

    Even more Javadocs

--- src/main/java/org/fcrepo/modeshape/FedoraIdentifiers.java
+++ src/main/java/org/fcrepo/modeshape/FedoraIdentifiers.java
@@ -1,5 +1,10 @@
 package org.fcrepo.modeshape;
 
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.DiscreteDomains.integers;
+import static com.google.common.collect.ImmutableSet.copyOf;
+import static com.google.common.collect.Ranges.closed;
+
 import java.io.IOException;
 
 import javax.jcr.RepositoryException;
@@ -10,27 +15,47 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 import freemarker.template.TemplateException;
 
+/**
+ * JAX-RS Resource offering PID creation.
+ * 
+ * @author ajs6f
+ *
+ */
 @Path("nextPID")
 public class FedoraIdentifiers extends AbstractResource {
 
+	/**
+	 * @param numPids
+	 * @return HTTP 200 with XML-formatted block of PIDs
+	 * @throws RepositoryException
+	 * @throws IOException
+	 * @throws TemplateException
+	 */
 	@POST
 	@Produces("text/xml")
 	public Response getNextPid(
 			@QueryParam("numPids") @DefaultValue("1") Integer numPids)
 			throws RepositoryException, IOException, TemplateException {
 
-		ImmutableSet.Builder<String> b = ImmutableSet.builder();
-		for (int i = 0; i < numPids; i++) {
-			b.add(pidMinter.mintPid());
-		}
+		ImmutableSet<String> pids = copyOf(transform(
+				closed(1, numPids).asSet(integers()), makePid));
+
 		return Response
 				.ok()
 				.entity(renderTemplate("nextPid.ftl",
-						ImmutableMap.of("pids", (Object) b.build()))).build();
+						ImmutableMap.of("pids", (Object) pids))).build();
 	}
+
+	private Function<Integer, String> makePid = new Function<Integer, String>() {
+		@Override
+		public String apply(Integer slot) {
+			return pidMinter.mintPid();
+		}
+	};
 }
