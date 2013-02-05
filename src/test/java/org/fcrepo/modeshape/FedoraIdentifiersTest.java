2d01ebff2cb4d7f054699b614dd60fc86134de47
ajs6f <ajs6f@virginia.edu>
2013-02-04 21:35:35 -0500

    Added a little more tests, because that's how we do what we do...

--- src/test/java/org/fcrepo/modeshape/FedoraIdentifiersTest.java
+++ src/test/java/org/fcrepo/modeshape/FedoraIdentifiersTest.java
@@ -2,12 +2,18 @@ package org.fcrepo.modeshape;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,17 +21,33 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("/testContext.xml")
 public class FedoraIdentifiersTest {
 
+	final private Logger logger = LoggerFactory
+			.getLogger(FedoraIdentifiersTest.class);
+
 	int SERVER_PORT = 9999;
 
 	@Test
-	public void testGetNextPid() throws Exception {
+	public void testGetNextPidResponds() throws Exception {
 		HttpClient client = new HttpClient();
 		PostMethod method = new PostMethod("http://localhost:" + SERVER_PORT
 				+ "/nextPID");
 		method.addRequestHeader("Accepts", "text/xml");
 		int status = client.executeMethod(method);
-		System.out.println("Executed testGetNextPid()");
-		System.out.println(method.getResponseBodyAsString());
+		logger.debug("Executed testGetNextPidResponds()");
 		assertEquals(HttpServletResponse.SC_OK, status);
+	}
+
+	@Test
+	public void testGetNextHasAPid() throws HttpException, IOException {
+		HttpClient client = new HttpClient();
+		PostMethod method = new PostMethod("http://localhost:" + SERVER_PORT
+				+ "/nextPID?numPids=1");
+		method.addRequestHeader("Accepts", "text/xml");
+		client.executeMethod(method);
+		logger.debug("Executed testGetNextHasAPid()");
+		String response = method.getResponseBodyAsString(Integer.MAX_VALUE);
+		logger.debug("Only to find:\n" + response);
+		assertEquals("Didn't find a single dang PID!", true,
+				Pattern.compile("<pid>.*?</pid>").matcher(response).find());
 	}
 }
